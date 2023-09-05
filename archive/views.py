410eda62197c1022c82057aa39828944c6bd8e56
harryhmx <harry4769@gmail.com>
2023-09-05 16:53:13 +0800

    Config for templates & static files of index page views

--- archive/views.py
+++ archive/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    print('{}: Request Home Page!'.format(request.get_host()))
+    template = loader.get_template('index.html')
+    context = {
+      'head_title': 'ReallyBraveHearts',
+      'body_title': 'Hello World',
+    }
+    return HttpResponse(template.render(context, request))
