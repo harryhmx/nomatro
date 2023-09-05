61f2790d572c0686b50dd528121805fa9c3dba8d
harryhmx <harry4769@gmail.com>
2023-09-05 23:00:42 +0800

    Config databases, make migrations and add posts page

--- archive/views.py
+++ archive/views.py
@@ -2,11 +2,19 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    print('{}: Request Home Page!'.format(request.get_host()))
     template = loader.get_template('index.html')
     context = {
       'head_title': 'ReallyBraveHearts',
       'body_title': 'Hello World',
+      'app_name':   'ReallyBraveHearts',
+    }
+    return HttpResponse(template.render(context, request))
+
+def posts(request):
+    template = loader.get_template('posts.html')
+    context = {
+      'head_title': 'ReallyBraveHearts',
+      'body_title': 'Posts Page',
       'app_name':   'ReallyBraveHearts',
     }
     return HttpResponse(template.render(context, request))
