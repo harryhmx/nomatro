b5824398adb421a0ec10a83a4fe46d20b27fc02e
harryhmx <harry4769@gmail.com>
2023-09-10 01:13:53 +0800

    Update layouts of home page and posts page, take RBH Programs as examples

--- archive/views.py
+++ archive/views.py
@@ -1,20 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
+from nomatro.config import APP_NAMES
+from .models import Post
 
 def index(request):
     template = loader.get_template('index.html')
     context = {
-      'head_title': 'ReallyBraveHearts',
-      'body_title': 'Hello World',
-      'app_name':   'ReallyBraveHearts',
+        'APP_NAMES':  APP_NAMES,
+        'head_title': APP_NAMES['MAIN'],
     }
     return HttpResponse(template.render(context, request))
 
 def posts(request):
     template = loader.get_template('posts.html')
     context = {
-      'head_title': 'ReallyBraveHearts',
-      'body_title': 'Posts Page',
-      'app_name':   'ReallyBraveHearts',
+        'APP_NAMES':  APP_NAMES,
+        'head_title': APP_NAMES['POSTS'],
+        'posts':      Post.objects.order_by('-created_at'),
     }
     return HttpResponse(template.render(context, request))
